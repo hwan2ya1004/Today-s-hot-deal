f5e306d22561408d8d0f017df35d69fce3afc038
hwan2ya1004 <71603769+hwan2ya1004@users.noreply.github.com>
2024-09-27 00:15:26 +0900

    Update hotdeal.js

--- netlify/functions/hotdeal.js
+++ netlify/functions/hotdeal.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const superagent = require('superagent');
 
 exports.handler = async function(event, context) {
     const apiUrl = 'https://api.adpick.co.kr/hotdeal?affiliateId=16d844';
 
     try {
-        // axios를 사용하여 API 호출
-        const response = await axios.get(apiUrl);
+        // Superagent를 사용하여 API 호출
+        const response = await superagent.get(apiUrl);
 
         // 성공적인 응답 반환
         return {
@@ -14,7 +14,7 @@ exports.handler = async function(event, context) {
                 "Access-Control-Allow-Origin": "*",  // CORS 문제 해결
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(response.data),
+            body: JSON.stringify(response.body),
         };
     } catch (error) {
         console.error('API 호출 중 오류 발생:', error.message);
