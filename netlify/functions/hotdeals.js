3a573e77197308b52a31e104ed057deda081f37c
hwan2ya1004 <71603769+hwan2ya1004@users.noreply.github.com>
2024-09-27 11:09:52 +0900

    Update hotdeals.js

--- netlify/functions/hotdeals.js
+++ netlify/functions/hotdeals.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 exports.handler = async function(event, context) {
     try {
         const response = await axios.get('https://adpick.co.kr/apis/sdk_shopping_hotdeal.php?affid=16d844');
+        console.log('ADPICK API 응답:', response.data); // 응답 데이터를 로그로 확인
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
         };
     } catch (error) {
+        console.error('ADPICK API 요청 실패:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ error: 'Failed to fetch data from ADPICK API' }),
